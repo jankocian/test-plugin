9876f86bf73acc9f6ba21a0d383256633b391103
jankocian <mail@jankocian.com>
2022-04-27 13:16:55 +0200

    Create test-plugin.php

---
+++ test-plugin.php
@@ -0,0 +1,8 @@
+/**
+ * Plugin Name:       Test Plugin
+ * Plugin URI:        https://github.com/jankocian/test-plugin
+ * Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
+ * Version:           1.0.0
+ * GitHub Plugin URI: jankocian/test-plugin
+ * Primary Branch:    main
+ */
